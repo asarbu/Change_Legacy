3ade1b25073c9f328d27c5ead2458b7b231815d7
uidt6622 <toolingroom.07_tm_fa@continental-corporation.com>
2024-01-30 14:03:06 +0200

    Finished implementing planning class

--- static/v2/js/persistence/planning.js
+++ static/v2/js/persistence/planning.js
@@ -1,55 +1,87 @@
+/**
+ * @class
+ */
 class Planning {
-    constructor() {
-        this.planningCollections = [];
-    }
-}
-
-class PlanningEntry {
-    constructor() {
-        /**
-         * @type{string}
-         */
-        this.planningEntryName = "";
-        this.planningCollections = [];
-    }
-}
-
-class PlanningCollection {
-    constructor() {
-        /**
-         * @type{string}
-         */
-        this.planningCollectionName = "";
-        /**
-         * @type{Array<PlanningCategory>}
-         */
-        this.planningCategories = [];
-    }
-}
-
-class PlanningCategory {
-    constructor() {
-        /**
-         * @type{string}
-         */
-        this.planningCategoryName = "";
-        this.planningEntries = [];
-    }
+	/**
+	 * @constructs Planning
+	 * @param {integer} year 
+	 */
+	constructor(year) {
+		this.year = year;
+		/**
+		 * @type{Array<PlanningContexts>}
+		 */
+		this.plannningContexts = [];
+	}
 }
 
 /**
  * @class
  */
-class PlanningUnit {
-    /**
-     * @constructs PlanningUnit
-     */
-    constructor() {
-        /**
-         * @type{integer}
-         */
-        this.daily = 0;
-        this.monthly = 0;
-        this.yearly = 0;
-    }
+class PlanningContext {
+	/**
+	 * 
+	 * @param {string} name 
+	 */
+	constructor(name) {
+		/**
+		 * @type{string}
+		 */
+		this.planningContextName = name;
+		/**
+		 * @type{Array<PlanningCategory>}
+		 */
+		this.planningCategories = [];
+	}
+}
+
+/**
+ * @class
+ */
+class PlanningCategory {
+	/**
+	 * 
+	 * @param {string} name 
+	 */
+	constructor(name) {
+		/**
+		 * @type{string}
+		 */
+		this.planningCategoryName = name;
+		/**
+		 * @type{Array<PlanningGoal>}
+		 */
+		this.planningGoals = [];
+	}
+}
+
+/**
+ * @class
+ */
+class PlanningGoal {
+	/**
+	 * @param {Object} goal - Unit to store in object.
+	 * @param {integer} goal.name - The name of the goal.
+	 * @param {integer} goal.daily - The name of the goal.
+	 * @param {integer} goal.monthly - The name of the goal.
+	 * @param {integer} goal.yearly - The name of the goal.
+	 */
+	constructor({ name, daily, monthly, yearly }) {
+		/**
+		 * @type{string}
+		 */
+		this.name = name;
+		/**
+		 * @type{integer}
+		 */
+		this.daily = daily;
+		/**
+		 * @type{integer}
+		 */
+		this.monthly = monthly;
+		/**
+		 * @type{integer}
+		 */
+		this.yearly = yearly;
+	}
 }
